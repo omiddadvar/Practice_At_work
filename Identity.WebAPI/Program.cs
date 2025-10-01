c16b4e92fcde3f211d01b78d8d4c31558d36c9d7
o.dadvar <o.dadvar@amnmoj.local>
2025-10-01 12:20:59 +0330

    refactor : Code-Clean-Up

--- Identity.WebAPI/Program.cs
+++ Identity.WebAPI/Program.cs
@@ -7,15 +7,13 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-//builder.Services.AddOpenApi();
+
 builder.Services.AddSwaggerGen();
 
 // Entity Framework
--- Identity.WebAPI/Services/TokenService.cs
+++ Identity.WebAPI/Services/TokenService.cs
@@ -1,12 +1,12 @@
-﻿using Identity.WebAPI.Abstractions.Services;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using Identity.WebAPI.Abstractions.Services;
 using Identity.WebAPI.Models.DTOs.Output;
 using Identity.WebAPI.Models.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Security.Cryptography;
-using System.Text;
 
 namespace Identity.WebAPI.Services;
 
