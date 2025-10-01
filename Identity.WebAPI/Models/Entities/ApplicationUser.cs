9be3d4dca0640f1e4716c8a8e1d79dfc049636b5
o.dadvar <o.dadvar@amnmoj.local>
2025-10-01 12:32:08 +0330

    refcator : Made-Refresh-Token-Nullable

--- Identity.WebAPI/Models/Entities/ApplicationUser.cs
+++ Identity.WebAPI/Models/Entities/ApplicationUser.cs
@@ -8,6 +8,6 @@ public class ApplicationUser : IdentityUser
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public string RefreshToken { get; set; }
-    public DateTime RefreshTokenExpiry { get; set; }
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiry { get; set; }
 }
