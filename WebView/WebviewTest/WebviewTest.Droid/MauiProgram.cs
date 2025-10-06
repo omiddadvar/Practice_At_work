1c92ac2429ed94589ef776510744c758f1e6fa3f
o.dadvar <o.dadvar@amnmoj.local>
2025-10-06 09:12:31 +0330

    feat : Adding-Webview-Testing-App

---
+++ WebView/WebviewTest/WebviewTest.Droid/MauiProgram.cs
@@ -0,0 +1,14 @@
+﻿namespace WebviewTest.Droid;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+
+        builder
+            .UseSharedMauiApp();
+
+        return builder.Build();
+    }
+}
---
+++ WebView/WebviewTest/WebviewTest.iOS/AppDelegate.cs
@@ -0,0 +1,8 @@
+using Foundation;
+
+namespace WebviewTest.iOS;
+[Register(nameof(AppDelegate))]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
